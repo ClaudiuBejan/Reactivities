a49d7493b6a283e879660c3c72bfb9909c250bac
ClaudiuBejan <claud.bejan@gmail.com>
2022-12-28 18:47:18 +0000

    End of Section 4

---
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,37 @@
+using Application.Activities;
+using Application.Core;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Extensions
+{
+    public static class ApplicationServiceExtensions
+    {
+        //this below refers to the instance of services, Iconfiguration provides access to 
+        //the configgurations in the json file. Only config needs to be passed in, as this is already there
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services,
+            IConfiguration config)
+        {
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+            services.AddDbContext<DataContext>(opt =>
+            {
+                opt.UseSqlite(config.GetConnectionString("DefaultConnectionString"));
+            });
+
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+                });
+            });
+            services.AddMediatR(typeof(List.Handler));
+            services.AddAutoMapper(typeof(MappingProfiles).Assembly);// use assembly to locate
+                                                                     // all mapping profiles in our application namespace
+            return services;
+        }
+    }
+}
